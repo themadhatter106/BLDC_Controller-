019067e43b2e3756ac5a42ea45c5c9a84d00ed8a
themadhatter106 <christopher_leger@student.uml.edu>
2015-02-17 17:19:02 -0500

    Code which blinks LED to test

---
+++ PIC_Test_Blinky_Light.c
@@ -0,0 +1,68 @@
+/* 
+ * File:   PIC_Test.c
+ * Author: Aetc
+ *
+ * Created on February 17, 2015, 3:28 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <PIC16F1788.h>
+
+
+// PIC16F1788 Configuration Bit Settings
+
+// 'C' source line config statements
+
+#include <xc.h>
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG1
+#pragma config FOSC = ECH       // Oscillator Selection (ECH, External Clock, High Power Mode (4-32 MHz): device clock supplied to CLKIN pin)
+#pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = ON        // Internal/External Switchover (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config VCAPEN = OFF     // Voltage Regulator Capacitor Enable bit (Vcap functionality is disabled on RA6.)
+#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = OFF      // Low Power Brown-Out Reset Enable Bit (Low power brown-out is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+
+
+
+int main(int argc, char** argv) {
+
+    OSCCON = 0b11111010;
+    PORTC = 0b00000000;
+    TRISC = 0b00000000;
+    int i;
+
+    while(1){
+
+
+        PORTCbits.RC3 = 1;
+        for(i=0; i<10000; i++);
+        PORTCbits.RC3 = 0;
+        for(i=0; i<10000; i++);
+
+    }
+
+
+
+
+    return (EXIT_SUCCESS);
+}
+
